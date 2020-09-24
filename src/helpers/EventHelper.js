c85dde18c178712307779eafbca7f27a34c63e18
Héctor Rodríguez <h.daidouji@gmail.com>
2020-09-24 18:23:15 -0500

    feat: add support for event grouping

--- src/helpers/EventHelper.js
+++ src/helpers/EventHelper.js
@@ -13,4 +13,38 @@ export default class EventHelper {
 
     return events.sort((a, b) => a.created_at.getTime() - b.created_at.getTime())
   }
+
+  /**
+   * Returns a grouped list of events
+   *
+   * @param {Array} events
+   * @returns {Array}
+   */
+  getGroupedEvents (events) {
+    const groupedEvents = { }
+
+    events.forEach(function (event) {
+      let month = '' + (event.created_at.getMonth() + 1)
+      let day = '' + event.created_at.getDate()
+      const year = event.created_at.getFullYear()
+
+      if (month.length < 2) {
+        month = '0' + month
+      }
+
+      if (day.length < 2) {
+        day = '0' + day
+      }
+
+      const date = [year, month, day].join('-')
+
+      if (!groupedEvents[date]) {
+        groupedEvents[date] = []
+      }
+
+      groupedEvents[date].push(event)
+    })
+
+    return groupedEvents
+  }
 }
