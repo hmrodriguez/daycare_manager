913870d9fd7736aa7ccfebfeb63db98579202475
Héctor Rodríguez <h.daidouji@gmail.com>
2020-09-24 12:55:53 -0500

    refactor: change from random word to lorem paragraph

--- src/models/ModelsFactory.js
+++ src/models/ModelsFactory.js
@@ -2,7 +2,7 @@ import factory from 'factoria'
 
 factory.define('Event', faker => ({
   type: faker.random.arrayElement(['accident', 'achievement']),
-  description: faker.random.words(),
+  description: faker.lorem.paragraph(),
   created_at: faker.date.past()
 }))
 
