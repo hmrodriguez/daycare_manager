ebdacd92cc90b22ae17045da8151037034fb49fb
Héctor Rodríguez <h.daidouji@gmail.com>
2020-09-24 18:28:53 -0500

    refactor: channged to a grouped events layout

--- src/views/Tutors.vue
+++ src/views/Tutors.vue
@@ -5,18 +5,24 @@
       <div class="col-3" role="col">
         <ul class="list-group">
           <li
-            v-for="(event, index) in events"
+            v-for="(date, index) in dateList"
             :key="index"
-            class="list-group-item"
-            :class="{active: selectedEvent == event}"
-            @click="selectedEvent = event"
+            class="list-group-item d-flex justify-content-between align-items-center"
+            @click="onDateClick(date)"
           >
-            {{event.created_at.toString().substring(0,25)}}
+            {{date.toString().substring(0,25)}}
+            <span class="badge badge-primary badge-pill" role="badge">
+              {{groupedEvents[date].length}}
+            </span>
           </li>
         </ul>
       </div>
-      <div class="col-9" role="col">
-        <event-card v-if="selectedEvent" :event="selectedEvent"/>
+      <div class="col-9" role="col" v-if="selectedEventDate">
+        <event-card
+          v-for="(event, index) in selectedEventGroup"
+          :key="index"
+          :event="event"
+        />
       </div>
     </div>
   </div>
@@ -25,6 +31,7 @@
 <script>
 import { mapGetters } from 'vuex'
 import EventCard from '../components/EventCard'
+import EventHelper from '../helpers/EventHelper'
 
 export default {
   components: {
@@ -33,18 +40,50 @@ export default {
 
   data () {
     return {
-      selectedEvent: null
+      selectedEvent: null,
+      selectedEventDate: null
     }
   },
 
   mounted () {
     this.selectedEvent = this.events[0]
+    this.selectedEventDate = this.dateList[0]
   },
 
   computed: {
     ...mapGetters({
       events: 'events'
-    })
+    }),
+
+    groupedEvents () {
+      const eventHelper = new EventHelper()
+
+      return eventHelper.getGroupedEvents(this.events)
+    },
+
+    dateList () {
+      const groupedEvents = this.groupedEvents
+
+      const dateList = Object.keys(groupedEvents).sort(function (a, b) {
+        a = new Date(a)
+        b = new Date(b)
+
+        return b.getTime() - a.getTime()
+      })
+
+      // return dateList.map(date => new Date(date))
+      return dateList
+    },
+
+    selectedEventGroup () {
+      return this.groupedEvents[this.selectedEventDate]
+    }
+  },
+
+  methods: {
+    onDateClick (date) {
+      this.selectedEventDate = date
+    }
   }
 }
 </script>
