5fd6a9835d517441432c5de56c9f2dfddf8b0492
Héctor Rodríguez <h.daidouji@gmail.com>
2020-09-24 12:28:21 -0500

    feat(tutors): initial tutor page

---
+++ src/views/Tutors.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container-fluid" role="container">
+    <h1 class="mb-5">Tutor Page</h1>
+    <div class="row" role="row">
+      <div class="col-3" role="col">
+        <ul class="list-group">
+          <li v-for="(event, index) in events" :key="index" class="list-group-item">
+            {{event.created_at.toString().substring(0,25)}}
+          </li>
+        </ul>
+      </div>
+      <div class="col-9" role="col">
+        One of three columns
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  computed: {
+    ...mapGetters({
+      events: 'events'
+    })
+  }
+}
+</script>
+
+<style lang="scss">
+  .container-fluid { text-align: left; }
+</style>
