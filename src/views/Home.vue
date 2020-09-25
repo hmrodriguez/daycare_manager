4901e650bad6e0f691bfb68bc0a80f58ae0a3e92
Héctor Rodríguez <h.daidouji@gmail.com>
2020-09-24 22:10:06 -0500

    refactor: remove hello world component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,12 @@
 <template>
-  <div class="home">
+  <div class="home" role="home-page">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="jumbotron mt-5 mb-5" role="jumbotron">
+      <h1 class="display-3">Daycare manager demo</h1>
+      <p class="lead">Keep a digital diary of achievements, incidents, activities, meals, naps and relevant events during the day by children or groups of children.</p>
+      <p class="lead">
+        <a class="btn btn-primary btn-lg" href="https://github.com/hmrodriguez/daycare_manager" role="button">GitHub repo</a>
+      </p>
+    </div>
   </div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
