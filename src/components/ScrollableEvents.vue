25d2fcde4df66664a2f92f617adcf540b41a6450
Héctor Rodríguez <h.daidouji@gmail.com>
2020-09-24 23:08:45 -0500

    feat: add scrollable event list

---
+++ src/components/ScrollableEvents.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="row justify-content-md-center" role="row">
+    <div class="col-12 col-md-8" role="col" v-if="items">
+      <event-card
+        v-for="(event, index) in items"
+        :key="index"
+        :event="event"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import EventHelper from '../helpers/EventHelper'
+import EventCard from '../components/EventCard'
+
+export default {
+  props: ['events'],
+
+  components: {
+    EventCard
+  },
+
+  data () {
+    return {
+      items: []
+    }
+  },
+
+  mounted () {
+    const eventHelper = new EventHelper()
+
+    this.items = eventHelper.sortEvents(this.events)
+  }
+}
+</script>
